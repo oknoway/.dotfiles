6b9f478ddafd4f01e581f90ec577980dd8d55cc9
Nate Bedortha <nate@oknoway.com>
2016-07-24 01:59:08 -0700

    New conversion script

--- zsh_custom_plugins/comix/cbr2cbz.sh
+++ zsh_custom_plugins/comix/cbr2cbz.sh
@@ -1,10 +1,21 @@
-cbr=$1
-start_directory=`pwd`
+#!/bin/bash
 
-cbz=`basename "$cbr" .cbr`.cbz
+if [ -z "$1" ]; then
+	echo "Convert cbr file to cbz file.";
+	echo "Usage:";
+	echo "    cbrtocbz {filenames}";
+else
+	for file in "$@"
+	do
+		dir=`mktemp -d`;
+		echo "Extracting images to $dir...";
+		7z e -o"$dir" "$file"
 
-tempdir=`mktemp -d`
-cd "$tempdir"
-unrar e -inul "$start_directory/$cbr"
-zip -q "$start_directory/$cbz" *
-rm -rf "$tempdir"
+		echo "Compressing images...";
+
+		name=$(echo $file | cut -f 1 -d '.');
+		zip -0 -j "$name".cbz "$dir"/*.[Jj][Pp][Gg] "$dir"/*.[Pp][Nn][Gg];
+
+		rm -rf "$dir"
+	done
+fi
