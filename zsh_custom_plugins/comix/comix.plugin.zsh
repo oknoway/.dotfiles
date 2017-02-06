a1293eb1259d308253b191ffd1fd52b06c2a0cf5
Nate Bedortha <nate@oknoway.com>
2017-02-06 08:15:13 -0800

    zsh_custom_plugins/comix work

--- zsh_custom_plugins/comix/comix.plugin.zsh
+++ zsh_custom_plugins/comix/comix.plugin.zsh
@@ -9,13 +9,13 @@ function rezip() {
 alias comictagger="comictagger.py"
 
 function checktags() {
-  TAGS="$(zsh comictagger -p -t cbl $@)"
+  TAGS="$(comictagger -p -t cbl $@)"
 
   if [ "${TAGS}" == '' ] ; then
-    echo "No tags on $@";
+    #echo "No tags on $@";
     return 1
   else
-    echo "Tags on $@";
+    #echo "Tags on $@";
     return 0
   fi
 }
@@ -23,19 +23,21 @@ function checktags() {
 function tagcomix() {
 
   HASTAGS="$(checktags $@)"
-  if HASTAGS ; then
-    zsh tag -a "Comic Tags" $@
+  if ${HASTAGS} ; then
+    $(tag -a "Comic Tags" $@)
     return
   fi
 
-  zsh comictagger -s --nooverwrite -t cbl -f -o -i -1 -w $@
+  $(comictagger -s --nooverwrite -t cbl -f -o -i -1 -w $@)
 
   HASTAGS="$(checktags $@)"
 
-  if HASTAGS ; then
-    zsh tag -a "Comic Tags" $@
+  if ${HASTAGS} ; then
+    $(tag -a "Comic Tags" $@)
+    $(tag -r "No Comic Tags" $@)
   else
-    zsh tag -a "No Comic Tags" $@
+    $(tag -a "No Comic Tags" $@)
+    $(tag -r "Comic Tags" $@)
   fi
 }
 
